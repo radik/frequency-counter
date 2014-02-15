09972ce52bd4bb92c3fc0af93748113191419e13
Radik Fattakhov <radikft@gmail.com>
2014-02-16 00:03:13 +0400

    Add order param.

--- src/main/java/me/radik/task/ValueComparator.java
+++ src/main/java/me/radik/task/ValueComparator.java
@@ -5,16 +5,31 @@ import java.util.Map;
 
 /**
  * Created by radik on 13/02/14.
+ * Comparator for sorting {@link java.util.Map} by value.
  */
 public class ValueComparator<T> implements Comparator<T> {
     private Map<T, Integer> _map;
+    private boolean _descend;
 
+    /**
+     * @param map Instance of {@link java.util.Map} which sorting by this comparator.
+     */
     public ValueComparator(Map<T, Integer> map) {
+        this(map, true);
+    }
+
+
+    /**
+     * @param map Instance of {@link java.util.Map} which sorting by this comparator.
+     * @param descend Order for sorting.
+     */
+    public ValueComparator(Map<T, Integer> map, boolean descend) {
         _map = map;
+        _descend = descend;
     }
 
     @Override
     public int compare(T key1, T key2) {
-        return _map.get(key2).compareTo(_map.get(key1));
+        return _descend ? _map.get(key2).compareTo(_map.get(key1)) : _map.get(key1).compareTo(_map.get(key2));
     }
 }
